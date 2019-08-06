b11d0be469bb41814a0b99225b09d75087d2abea
BIT <BIT@DESKTOP-Q04GE57>
2019-08-06 16:03:40 +0900

    1번 완료

--- src/practice01/prob1/Prob1.java
+++ src/practice01/prob1/Prob1.java
@@ -1,6 +1,18 @@
 package practice01.prob1;
 
+import java.util.*;
+
 public class Prob1 {
 	public static void main (String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("수를 입력하시오 : ");
+		int i = sc.nextInt();
+		
+		if(i % 3 == 0) {
+			System.out.println("3의 배수입니다.");
+		}
+		
+		sc.close();
 	}
 }
