13fcf6daad7636f996a66dc1e47b19a40bd5beab
BIT <BIT@DESKTOP-Q04GE57>
2019-08-06 16:28:03 +0900

    5번 완료

--- src/practice01/prob5/Prob5.java
+++ src/practice01/prob5/Prob5.java
@@ -1,6 +1,25 @@
 package practice01.prob5;
 
+import java.util.*;
+
 public class Prob5 {
 	public static void main (String args[]) {
+		for(int i = 1; i <= 99; i++) {
+			int count = 0;
+			
+			String[] s = Integer.toString(i).split("");
+			
+			for(int j = 0; j < s.length; j++) {
+				if(Integer.parseInt(s[j]) % 3 == 0 && Integer.parseInt(s[j]) != 0) {
+					count++;
+				}
+			}
+			
+			if(count == 1) {
+				System.out.println(i + " 짝");
+			} else if(count == 2) {
+				System.out.println(i + " 짝짝");
+			}
+		}
 	}
 }
