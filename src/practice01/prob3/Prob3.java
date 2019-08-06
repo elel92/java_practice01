28def276268948020fabff0c1ae91151a627c6ec
BIT <BIT@DESKTOP-Q04GE57>
2019-08-06 16:13:59 +0900

    3번 완료

--- src/practice01/prob3/Prob3.java
+++ src/practice01/prob3/Prob3.java
@@ -1,6 +1,26 @@
 package practice01.prob3;
 
+import java.util.*;
+
 public class Prob3 {
 	public static void main (String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자를 입력하세요 : ");
+		int input = sc.nextInt();
+		
+		int answer = 0;
+		
+		if(input % 2 == 0) {
+			for(int i = 0; i <= input; i=i+2) {
+				answer += i;
+			}
+		} else {
+			for(int i = 1; i <= input; i=i+2) {
+				answer += i;
+			}
+		}
+		
+		System.out.println("결과 값 : " + answer);
 	}
 }
